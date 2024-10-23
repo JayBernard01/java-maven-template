948cd572f5814d36b7963f940cc8f41655557450
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-10-23 00:00:34 -0400

    clean setup

--- application/src/test/java/ca/ulaval/glo4002/template/application/DumbClassTest.java
+++ application/src/test/java/ca/ulaval/glo4002/template/application/DumbClassTest.java
@@ -1,9 +1,9 @@
 package ca.ulaval.glo4002.template.application;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DumbClassTest {
 
